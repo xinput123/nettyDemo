367de7bfe6150be038785763da12c3d77837a848
xinput <yuan.lai@hellogroup.com>
2025-11-26 23:58:30 +0800

    粘包拆包

---
+++ src/main/java/com/xinput/ch03/client/BaseClient03.java
@@ -0,0 +1,46 @@
+package com.xinput.ch03.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * 单次发送的包内容过多的情况，拆包
+ */
+public class BaseClient03 {
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8080"));
+    static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new StringDecoder());
+                            p.addLast(new BaseClientHandler());
+                        }
+                    });
+
+            ChannelFuture future = b.connect(HOST, PORT).sync();
+            future.channel().writeAndFlush("Hello Netty Server ,I am a common client");
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
